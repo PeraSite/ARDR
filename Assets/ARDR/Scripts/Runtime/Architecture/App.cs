195abe38cce93048c1ba24a08a74b4dedabe69a6
PeraSIte <pretocki3@gmail.com>
2022-05-26 23:41:26 +0900

    LeanTouch 추가 App 아키텍처 적용

--- Assets/ARDR/Scripts/Runtime/Architecture/App.cs
+++ Assets/ARDR/Scripts/Runtime/Architecture/App.cs
@@ -9,7 +9,7 @@ public static class App {
 	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
 	private static void Bootstrap() {
 		var app = Object.Instantiate(Resources.Load("App"));
-
+		app.name = "App";
 		if (app.SafeIsUnityNull()) {
 			throw new ApplicationException("Can't find main bootstrap App");
 		}
